5ceee6e15145fae551fe6f6dcf67c157628a9ee5
Kushal <vankadara2004@gmail.com>
2024-06-20 00:22:46 +0530

    Movie data added

---
+++ src/MovieData.js
@@ -0,0 +1,32 @@
+export const movies=[
+  {
+    title: "The Avengers",
+    plot: "The plot revolves around Nick Fury, director of the peacekeeping agency S.H.I.E.L.D., who assembles a team of superheroes to save the world from impending disaster.",
+    price: "199",
+    rating: "8.9",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRednvInaxl1XBf35RK4RtEAIbVHA9p3S4YYA&s",
+    stars: 0,
+    favorite: false,
+    cart: false
+  },
+  {
+    title: "The IronMan",
+    plot: "The plot revolves around Tony Stark, a brilliant and wealthy industrialist and engineer, who is kidnapped by terrorists while demonstrating a new missile in Afghanistan.",
+    price: "299",
+    rating: "9.0",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjRF77eX5id7Z1_G8uhkQ7MvhBoGksNufViA&s",
+    stars: 0,
+    favorite: false,
+    cart: false
+  },
+  {
+    title: "Doraemon",
+    plot: "The plot revolves around a robotic cat named Doraemon, who travels back in time from the 22nd century to help a young boy named Nobita Nobi.",
+    price: "399",
+    rating: "9.2",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4iPRa5CUIXgeOj_SIMqwozV848VHePY1fUQ&s",
+    stars: 0,
+    favorite: false,
+    cart: false
+  }
+]
